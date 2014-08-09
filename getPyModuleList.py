da47eb55c5ec5c2845256a3b0f56cb77ba01213c
hysuh <yuvika.suvarna@gmail.com>
2014-08-09 21:09:40 +0100

    Updated the description

--- getPyModuleList.py
+++ getPyModuleList.py
@@ -1,5 +1,7 @@
 '''
-Copyright(c) 2014 YSUV - Fisher yates shuffle
+Copyright(c) 2014 YSUV - This script either lists the modules supported by
+a python version or it checks whether a module is supported by the python 
+version
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
