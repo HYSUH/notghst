4790fba387050813e340cf176af0bf5dab56611f
hysuh <yuvika.suvarna@gmail.com>
2014-08-09 15:36:33 +0100

    updated comment

--- logoGenerator.py
+++ logoGenerator.py
@@ -1,5 +1,5 @@
 '''
-Copyright(c) 2014 YSUV - Fisher yates shuffle
+Copyright(c) 2014 YSUV - MultiColour Square generator
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
