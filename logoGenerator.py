a17124e37a9d66cd3d6584a5eac696f479e08076
hysuh <yuvika.suvarna@gmail.com>
2014-08-09 15:21:25 +0100

    Generate multi-coloured squares using the python turtle module

---
+++ logoGenerator.py
@@ -0,0 +1,94 @@
+'''
+Copyright(c) 2014 YSUV - Fisher yates shuffle
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to
+do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+import turtle
+import random
+
+
+class MultiColoredSquares(turtle.Turtle):
+
+    def __init__(self, startx=300, starty=50, squareSize=20,
+                 length=30, width=5):
+        '''
+        (startx, startx)  - the x and y coordinates on the screen that the
+        turtle starts drawing.
+        squareSize  - the size of the square drawn.
+        length  - the number of squares drawn horizontally
+        width - number of quares drwan vertically.
+        '''
+        self.startx = startx
+        self.starty = starty
+        self.squareSize = squareSize
+        self.length = length
+        self.width = width
+        super(MultiColoredSquares, self).__init__()
+
+    def turnRight(self):
+        ''' Turns the turtle right by rotating 90 degrees '''
+        self.right(90)
+
+    def setPosToDrawSquareLine(self, i):
+        ''' This function moves the turtle to the position so that the set of
+        squares can be drawn'''
+        self.penup()
+        self.setx(self.startx)
+        self.sety(self.starty - self.squareSize * i)
+
+    def moveBackASquare(self):
+        ''' This function moves the turtle back so that the next square can be drawn
+        again '''
+        self.penup()  # dont want to draw while moving back
+        self.backward(self.squareSize)
+
+    def drawFilledSquare(self):
+        ''' This function draws a square that is colored using a randomly
+        selected color
+        '''
+        self.pendown()
+        self.color(random.random(), random.random(), random.random())
+        self.begin_fill()
+        for _ in xrange(4):
+            self.forward(self.squareSize)
+            self.turnRight()
+        self.end_fill()
+
+    def drawMultiSquareLine(self):
+        ''' draws `length` number of squares horizontally starting at (x,y) and
+        moving backwards '''
+        for _ in xrange(self.length):
+            self.moveBackASquare()
+            self.drawFilledSquare()
+
+    def createSquareLogo(self):
+        ''' This function draws a mulitcoloured line `width` number of times
+        '''
+        self.reset()
+        for i in xrange(self.width):
+            self.setPosToDrawSquareLine(i)
+            self.drawMultiSquareLine()
+
+if __name__ == '__main__':
+    try:
+        testturtle = MultiColoredSquares()
+        testturtle.createSquareLogo()
+    except Exception as e:
+        print e
